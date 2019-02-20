7385e6966a0c485ea3d4f071144f8def2997f78a
iqeq00 <iqeq00@163.com>
2019-02-20 15:13:28 +0800

    Add node grpc example with dynamic mode

---
+++ examples/node/app/dynamic/grpcClient.js
@@ -0,0 +1,24 @@
+// PROTO_PATHvar PROTO_PATH = "C:/Lichee/Develop/workspace/webstorm/grpc-node/proto/Student.proto";
+// var grpc = require("grpc");
+// var grpcService = grpc.load(PROTO_PATH).grpc;
+
+//run command: node app/dynamic/grpcClient.js
+var PROTO_PATH = __dirname + '/../../../../src/main/proto/Student.proto';
+var grpc = require('grpc');
+var protoLoader = require('@grpc/proto-loader');
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true
+    });
+var grpcService = grpc.loadPackageDefinition(packageDefinition).grpc;
+
+var client = new grpcService.StudentService("localhost:8899", grpc.credentials.createInsecure());
+client.getRealnameByUsername({username: "Node Client: 里奇"}, function (error, respData) {
+    console.log("收到服务器的返回信息: " + respData.realname);
+    grpc.closeClient(client);
+});
---
+++ examples/node/app/dynamic/grpcServer.js
@@ -0,0 +1,46 @@
+// PROTO_PATHvar PROTO_PATH = "C:/Lichee/Develop/workspace/webstorm/grpc-node/proto/Student.proto";
+// var grpc = require("grpc");
+// var grpcService = grpc.load(PROTO_PATH).grpc;
+
+//run command: node app/dynamic/grpcServer.js
+var PROTO_PATH = __dirname + '/../../../../src/main/proto/Student.proto';
+var grpc = require("grpc");
+var protoLoader = require('@grpc/proto-loader');
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true
+    });
+var grpcService = grpc.loadPackageDefinition(packageDefinition).grpc;
+
+var server = new grpc.Server();
+server.addService(grpcService.StudentService.service, {
+    getRealnameByUsername: getRealnameByUsername,
+    getStudentsByAge: getStudentsByAge,
+    getStudentsWrapperByAges: getStudentsWrapperByAges,
+    biTalk: biTalk
+});
+server.bind("localhost:8899", grpc.ServerCredentials.createInsecure());
+server.start();
+console.log("grpc node server start!")
+
+function getRealnameByUsername(call, callback) {
+    console.log("收到客户端的请求信息: " + call.request.username);
+    callback(null, {realname: "Node Server: 里奇"});
+}
+
+function getStudentsByAge() {
+
+}
+
+function getStudentsWrapperByAges() {
+
+}
+
+function biTalk() {
+
+}
