0cac111f1e852e6f5677ef42ab83948e58d41139
iqeq00 <iqeq00@gmail.com>
2022-08-18 10:50:05 +0800

    Modify MainTest class

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,10 @@
+import java.util.UUID;
+
+public class MainTest {
+
+    public static void main(String[] args) {
+
+        String uuid = UUID.randomUUID().toString();
+        System.out.println(uuid);
+    }
+}
